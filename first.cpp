9d170ad12148c86cee170063e807d9bc8a7df854
Sumantra27 <sumantra2710@gmail.com>
2026-06-18 12:41:54 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -426,8 +426,15 @@ int main() {
     }    
 }
 
+
+///////
+pattern->
+A B C D 
+B C D E 
+C D E F 
+D E F G 
  
-*/
+
 
 #include<iostream>
 using namespace std;
@@ -439,7 +446,7 @@ int main() {
     while(row <= n) { 
         int col = 1;   
         while(col <= n) {
-            char ch = 'A' + col - 1;
+            char ch = 'A' +row + col - 2 ;
             cout << ch << " ";
             col = col + 1;
         }
@@ -448,6 +455,85 @@ int main() {
     }    
 }
  
+////////
+pattern- 
+A 
+B B 
+C C C 
+D D D D 
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter value n: ";
+    cin >> n;
+    int row = 1;
+    while(row <= n) { 
+        int col = 1;   
+        while(col <= row) {
+            char ch = 'A' + row - 1;
+            cout << ch << " ";
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }    
+}
+  
+//////////////
+pattern
+Enter value n: 5
+A 
+B C 
+C D E 
+D E F G 
+E F G H I 
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter value n: ";
+    cin >> n;
+    int row = 1;
+    while(row <= n) { 
+        int col = 1;    
+        while(col <= row) {
+            char ch = 'A' + row + col - 2;
+            cout << ch << " ";
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }    
+}
   
 
+/////////
+pattern- 
 
+
+*/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter value n: ";
+    cin >> n;
+    int row = 1;
+    while(row <= n) { 
+        int col = 1;    
+        while(col <= row) {
+            char ch = 'A' + row + col - 2;
+            cout << ch << " ";
+            col = col + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }    
+}
+  
